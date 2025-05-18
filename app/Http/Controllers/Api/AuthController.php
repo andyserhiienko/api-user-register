f94c8d7b26f1afd58420353a7752d87332f65b20
andy <andyserhiienko@gmail.com>
2025-05-18 18:28:37 +0300

    Create AuthComtroller

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request)
+    {
+        $validate = $request->validate([
+            'email' => 'required|email|unique',
+            'password' => 'required|min:6',
+            'gender' => 'required|in:male,female,other'
+        ]);
+
+        $user = User::create([
+            'email' => $validated['email'],
+            'password' => bycrypt($validated['password'])
+            'gender' => $validated['gender'],
+        ]);
+
+        $token = $user->createToken('api_token')->plainTextToken();
+        
+        return response()->json([
+            'message' => 'Success',
+            'user' => $user,
+            'token' => $token
+        ],201);
+    }
+
+}
